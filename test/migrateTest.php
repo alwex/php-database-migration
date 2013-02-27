cdfc790ce7fd6b5393c1b7871c0ac393a49c5bc8
Alexandre GUIDET <aguidet@hi-media.com>
2013-02-27 14:00:08 +0100

    first testcase

--- test/migrateTest.php
+++ test/migrateTest.php
@@ -1,6 +1,7 @@
 <?php 
-class MigrateTest extends PHPUnit_Framework_TestCase {
-
+class MigrateTest extends PHPUnit_Framework_TestCase 
+{
+    
 
     public function execute($cmd)
     {
@@ -43,6 +44,27 @@ class MigrateTest extends PHPUnit_Framework_TestCase {
     public function statusTest()
     {
         $this->assertContains("description", $this->execute("./migrate --status --env=test"));
+    }
+
+    /**
+     * @test
+     */
+    public function up_aNewMigration()
+    {
+        $migrationOutput = $this->execute("./migrate --generate my_first_migration");
+        $migrationOutput = str_replace("\033[1;32mmigration: ", "", $migrationOutput);
+        $migrationOutput = str_replace("\033[0m", "", $migrationOutput);
+
+        $myFirstMigration =<<<M0
+--// my first migration
+-- Migration SQL that makes the change goes here.
+select 1;
+--//@UNDO\n-- SQL to undo the change goes here.
+select 0;
+M0;
+        file_put_contents("../" . $migrationOutput, $myFirstMigration);
+
+        $this->assertContains("Pending...           my first migration", $this->execute("./migrate --status --env=test"));
     }
 }
 ?>
