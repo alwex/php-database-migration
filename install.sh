74a554dfacf4d8b8d5367f3c8c30c94a44b382f7
Alexandre GUIDET <guidet.alexandre@gmail.com>
2015-02-28 22:14:43 +0100

    deletion of old unused files

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-user=`whoami`
-completion_path=`pwd`
-sudo ln -s $completion_path/install/migrate_bash_completion.sh /etc/bash_completion.d/migrate
-echo "to complete install run:"
-echo "source ~/.bashrc"
--- tests/MigratorTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once dirname(__FILE__) . '/../Migrator.php';
-
-class MigratorTest extends PHPUnit_Framework_TestCase
-{
-    public function testTrue() {
-        $this->assertTrue(true);
-    }
-}
