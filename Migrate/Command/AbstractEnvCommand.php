bd3604659c6f25f215f419540012e7edf16919d2
Alexandre Guidet <alex@room9.co.nz>
2016-09-16 15:04:27 +1200

    add dump for debug on travis

--- Migrate/Command/AbstractEnvCommand.php
+++ Migrate/Command/AbstractEnvCommand.php
@@ -75,7 +75,7 @@ class AbstractEnvCommand extends AbstractComand
         $conf = $loader->parse($envFile);
 
         $this->config = $conf;
-
+var_dump($conf);
         $driver = ArrayUtil::get($conf['connection'], 'driver');
         $port = ArrayUtil::get($conf['connection'], 'port');
         $host = ArrayUtil::get($conf['connection'], 'host');
